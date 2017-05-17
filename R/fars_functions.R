f1609c95ee2e9a28b2de4a66b86818479f3286c8
EnriquePH <eph.project1500@gmail.com>
2017-05-17 21:58:11 +0200

    NSE2

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -32,15 +32,13 @@
 #'   file does not exists.
 #'
 #' @examples
-#' \dontrun{
 #' library(dplyr)
 #' library(readr)
 #' yr <- 2015
 #' data <- yr %>%
 #'   make_filename %>%
 #'   fars_read
-#' View(data)
-#' }
+#' head(data)
 #' @note To generate file name use: \code{\link{make_filename}}
 #' @seealso \link{make_filename}
 #' @export
@@ -62,16 +60,18 @@ fars_read <- function(filename) {
 #' @param year A string or an integer with the input \code{year}
 #'
 #' @return This function returns a string with the data file name for a given
-#'   year
+#'   year, and the file path within the package.
 #'
 #' @examples
 #' make_filename(2013)
-#' # "accident_2013.csv.bz2"
 #' @seealso \link{fars_read}
 #' @export
 make_filename <- function(year) {
         year <- as.integer(year)
-        sprintf("accident_%d.csv.bz2", year)
+        system.file("extdata",
+                    sprintf("accident_%d.csv.bz2", year),
+                    package = "fars",
+                    mustWork = TRUE)
 }
 
 #' Read FARS years
@@ -90,18 +90,15 @@ make_filename <- function(year) {
 #' @seealso \link{make_filename}
 #' @seealso \link{fars_summarize_years}
 #' @examples
-#' \dontrun{
 #' fars_read_years(2013)
-#' }
 #' @export
 fars_read_years <- function(years) {
-  MONTH = NULL # Avoid no visible binding for global variable note
         lapply(years, function(year) {
-                file <- make_filename(year)
+          file <- make_filename(year)
                 tryCatch({
                         dat <- fars_read(file)
-                        dplyr::mutate_(dat, year = year) %>%
-                                dplyr::select_(MONTH, year)
+                        dplyr::mutate_(dat,  year = "YEAR") %>%
+                          dplyr::select_("MONTH", "year")
                 }, error = function(e) {
                         warning("invalid year: ", year)
                         return(NULL)
@@ -122,18 +119,15 @@ fars_read_years <- function(years) {
 #' @importFrom magrittr "%>%"
 #' @seealso \link{fars_read_years}
 #' @examples
-#' \dontrun{
 #' plot(fars_summarize_years(2015))
 #' fars_summarize_years(c(2015, 2014))
-#' }
 #' @export
 fars_summarize_years <- function(years) {
-  year = MONTH = n = NULL # Avoid no visible binding for global variable note
   dat_list <- fars_read_years(years)
   dplyr::bind_rows(dat_list) %>%
-    dplyr::group_by_(year, MONTH) %>%
-    dplyr::summarize_(n = n()) %>%
-    tidyr::spread_(year, n)
+    dplyr::group_by_("year", "MONTH") %>%
+    dplyr::summarize_(n = "n()") %>%
+    tidyr::spread_("year", "n")
 }
 
 #' Display accidents map by state and year
@@ -212,23 +206,22 @@ fars_summarize_years <- function(years) {
 #' }
 #' @export
 fars_map_state <- function(state.num, year) {
-  STATE = NULL # Avoid no visible binding for global variable note
-        filename <- make_filename(year)
-        data <- fars_read(filename)
-        state.num <- as.integer(state.num)
-
-        if(!(state.num %in% unique(data$STATE)))
-                stop("invalid STATE number: ", state.num)
-        data.sub <- dplyr::filter_(data, STATE == state.num)
-        if(nrow(data.sub) == 0L) {
-                message("no accidents to plot")
-                return(invisible(NULL))
-        }
-        is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
-        is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
-        with(data.sub, {
-                maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
-                          xlim = range(LONGITUD, na.rm = TRUE))
-                graphics::points(LONGITUD, LATITUDE, pch = 46)
-        })
+  filename <- make_filename(year)
+  data <- fars_read(filename)
+  state.num <- as.integer(state.num)
+  if(!(state.num %in% unique(data$STATE))) {
+    stop("invalid STATE number: ", state.num)
+  }
+  data.sub <- dplyr::filter_(data, .dots = paste0("STATE==", state.num))
+  if(nrow(data.sub) == 0L) {
+    message("no accidents to plot")
+    return(invisible(NULL))
+  }
+  is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
+  is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
+  with(data.sub, {
+    maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
+              xlim = range(LONGITUD, na.rm = TRUE))
+    graphics::points(LONGITUD, LATITUDE, pch = 46)
+  })
 }
